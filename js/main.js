afe36f7f35172bc386a764bb12a564026db65a0d
justingreco <justin.greco@raleighnc.gov>
2015-06-24 16:23:51 -0400

    changing from getlayers on eachFeature

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var map, geojson, locationGroup, materials, point;
+var map, geojson, locationGroup, materials, point, facilities;
 
 //Result - Small Blue Point - for Geolocated or Searched Location
 function addLocationToMap(latlng) {
@@ -123,7 +123,7 @@ function findNearest(latlng) {
 		tbody = $("table tbody").empty(),
 		distances = [];
 	point = latlng
-  $(geojson.getLayers()).each(function(i, l) {
+  facilities.eachFeature(function(i, l) {
 		if ($.inArray(l.feature.properties.category, placeTypes) > -1) {
 			var coords = L.latLng(l.feature.geometry.coordinates[1], l.feature.geometry.coordinates[0]),
 				dist = latlng.distanceTo(coords);
@@ -164,7 +164,7 @@ function createMap() {
 
   map = L.map('map').setView([35.81889, -78.64447], 10);
   L.esri.basemapLayer('Gray').addTo(map);
-  var facilities = L.esri.featureLayer('http://maps.wakegov.com/arcgis/rest/services/Environmental/SWFacilities/MapServer/0', {
+  facilities = L.esri.featureLayer('http://maps.wakegov.com/arcgis/rest/services/Environmental/SWFacilities/MapServer/0', {
   	onEachFeature: function (feature) {
 	$("table tbody").append("<tr><td>"+feature.properties.OPERATOR+"</td><td>"+feature.properties.TYPE+"</td><td>"+feature.properties.ADDRESS+"</td><td>"+feature.properties.HOURS+"</td><td>"+feature.properties.OPENTO+"</td><td></td></tr>");
   }
@@ -172,6 +172,8 @@ function createMap() {
   facilities.bindPopup(function (feature) {
   	return L.Util.template('<strong>{OPERATOR}</strong><br/>{TYPE}<br/>{ADDRESS}<br/>{HOURS}<strong><br/>{OPENTO}', feature.properties);
   });
+
+
 
   //L.tileLayer('http://server.arcgisonline.com/arcgis/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}').addTo(map);
   // $.getJSON("facilities.geojson", function (data) {
